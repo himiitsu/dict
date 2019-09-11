48c6fb073843bf9a340d5c9eb7e8c3261b20b251
Danil <dts.msk@gmail.com>
2019-09-11 17:05:51 +0300

    Update

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import random
 import sys
 
+#Выбираем случайный индекс за исключением индекса n
+def excludingRandom(out, n):
+    length = len(out)
+    res = list(range(0, n)) + list(range(n + 1, length))
+    return random.choice(res)
+
 UP = (0, 1)
 LEFT = (-1, 0)
 RIGHT = (1, 0)
@@ -19,10 +25,29 @@ while i < width:
     result.append([])
     i = i + 1
 
+i = 0
+
+#заполняем всю матрицу элементом неопределённости "А"
 while j <= length:
-    result[j].append(random.choice(states))
+    while i <= width:
+        result[j].append("A")
+        i = i + 1
+    j = j + 1
+    i = 0
+
+
+
+result[0][0] = states[2]
+i = 0
+j = 0
+while j <= length:
+    if result[0][0] == 'S':
+        result[j][i+1] = states[excludingRandom(states, 0)]
+        j = j + 1
+        continue
     result[j].append(random.choice(states))
     j = j + 1
+
 
 i = 0
 while i < len(result):
